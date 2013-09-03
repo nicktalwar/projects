5fc17624da440eb004d1c781d972cef7807c156c
nicktalwar <ram.talwar@gmail.com>
2013-09-03 01:48:10 -0700

    Added some jQuery in custom.js to simply on mouseover of an image, place some CSS overlay (see style.css) on top of it. Doesn't work for some reason, getting late so will try again in morning. Also commented out a CSS rollover approach (style.css) that I have not tried yet.

--- _js/custom.js
+++ _js/custom.js
@@ -1,1 +1,11 @@
 // Custom.JS all custom JS goes in here to extend jQuery and bootstrap.js
+
+$(function(){
+
+	$('.wrap-mouseover').mouseover(function () {
+		$('.image-overlay').show();
+	}).mouseout(function () {
+		$('.image-overlay').hide();
+	});
+
+});
